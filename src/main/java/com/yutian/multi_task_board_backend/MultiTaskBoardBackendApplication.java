f36f90b232373a0ff66263b36b4ecfa2b43a2c41
VicTian1 <ty10322022@126.com>
2026-07-23 19:27:51 +0800

    Initialize Spring Boot backend project

---
+++ src/main/java/com/yutian/multi_task_board_backend/MultiTaskBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yutian.multi_task_board_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTaskBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTaskBoardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yutian/multi_task_board_backend/MultiTaskBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutian.multi_task_board_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTaskBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
